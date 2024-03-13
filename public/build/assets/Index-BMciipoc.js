7d8e187c4ef04f6ea90456ed85de71cffcad93c0
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-03-13 17:42:48 -0500

    update assets

--- public/build/assets/Index-BMciipoc.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BCCgbaEy.js";import{A as r}from"./AuthenticatedLayout-CuX9Xg-P.js";import"./transition-b3MWsbJg.js";function i(s){const{auth:e}=s;return t.jsx(r,{user:e.user,children:t.jsx("h1",{className:"title",children:"Resultados"})})}export{i as default};
---
+++ public/build/assets/Index-DmcpZ7_U.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Fk2n4M-4.js";import{A as e}from"./AuthenticatedLayout-ChepMe3j.js";import"./transition-D6FghL7g.js";function n(t){const{auth:a}=t;return r.jsx(e,{user:a.user,children:r.jsx("h1",{className:"title",children:"Programación"})})}export{n as default};
