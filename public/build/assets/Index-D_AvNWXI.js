d123973014a190873a55236307a069f5a1a70ba2
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-03-13 20:57:26 -0500

    update assets

---
+++ public/build/assets/Index-D_AvNWXI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DL6SszUQ.js";import{A as e}from"./AuthenticatedLayout-CkIwE4L3.js";import"./transition-DVVd3TgL.js";function n(t){const{auth:a}=t;return r.jsx(e,{user:a.user,children:r.jsx("h1",{className:"title",children:"Programación"})})}export{n as default};
