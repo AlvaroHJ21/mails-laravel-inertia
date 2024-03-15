df86fee2be6439b8223b97d67d62c3d6ecc87b08
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-03-14 21:22:33 -0500

    update assets

---
+++ public/build/assets/Index-BktdPYN6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-DtAwit40.js";import{A as r}from"./AuthenticatedLayout-W4UHbfJJ.js";import"./transition-CxG8041q.js";function i(s){const{auth:e}=s;return t.jsx(r,{user:e.user,children:t.jsx("h1",{className:"title",children:"Resultados"})})}export{i as default};
--- public/build/assets/Index-C8V85VaZ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-DL6SszUQ.js";import{A as r}from"./AuthenticatedLayout-CkIwE4L3.js";import"./transition-DVVd3TgL.js";function i(s){const{auth:e}=s;return t.jsx(r,{user:e.user,children:t.jsx("h1",{className:"title",children:"Resultados"})})}export{i as default};
