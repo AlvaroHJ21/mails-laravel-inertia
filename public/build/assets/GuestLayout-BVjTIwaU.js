e99a018efaa2fddaa09bce0bd953aaa752040ae1
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-03-26 11:45:29 -0500

    update assets

---
+++ public/build/assets/GuestLayout-BVjTIwaU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DMpw0c6V.js";function m({children:s}){return e.jsx("div",{className:"flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-auth-gradient",children:e.jsx("div",{className:"relative w-full px-6 pt-12 pb-6 bg-white shadow-md sm:max-w-md sm:rounded-lg",children:s})})}export{m as G};
